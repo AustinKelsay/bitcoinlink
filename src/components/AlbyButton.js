a65e7f285ed4fef6ca4fc1298d39d3963a3a467b
austinkelsay <austinkelsay@yahoo.com>
2024-05-04 12:27:39 -0500

    revert db connection change, dark mode only

--- src/components/AlbyButton.js
+++ src/components/AlbyButton.js
@@ -16,9 +16,9 @@ const AlbySVG = () => (
     
 );
 
-const AlbyButton = ({ handleSubmit }) => {
+const AlbyButton = ({ handleSubmit, disabled }) => {
   return (
-    <Button className="p-button-success hover:opacity-75" style={{ backgroundColor: '#FFDE6E', borderColor: '#FFDE6E', padding: '10px 20px' }} onClick={handleSubmit}>
+    <Button disabled={disabled ? true : false} className="p-button-success hover:opacity-75" style={{ backgroundColor: '#FFDE6E', borderColor: '#FFDE6E', padding: '10px 20px' }} onClick={handleSubmit}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px' }}>
         <AlbySVG style={{ width: '21px', height: '22px' }} />
         <span style={{ color: 'black' }}>Generate with Alby</span>
