737bb7bc01fff8f091ca7f8c988752025538572c
austinkelsay <austinkelsay@yahoo.com>
2024-05-04 19:02:39 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 p-4 fixed bottom-0 w-full">
+        <footer className="bg-gray-800 p-2 fixed bottom-0 w-full">
             <div className="container mx-auto text-center text-white">
                 <p className="text-sm">
                     BitcoinLink is open source. View the source code <a href="https://github.com/AustinKelsay/bitcoinlink" target="_blank" rel="noopener noreferrer">here</a>.
