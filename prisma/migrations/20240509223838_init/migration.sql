3ea62d6cd3249830a65691995ccfc6eed09262c3
austinkelsay <austinkelsay@yahoo.com>
2024-05-09 17:39:04 -0500

    link index back to uuid

--- prisma/migrations/20240509223838_init/migration.sql
+++ prisma/migrations/20240509223838_init/migration.sql
@@ -12,12 +12,15 @@ CREATE TABLE "NWC" (
 -- CreateTable
 CREATE TABLE "Link" (
     "id" TEXT NOT NULL,
-    "linkIndex" INTEGER NOT NULL,
+    "linkIndex" TEXT NOT NULL,
     "nwcId" TEXT NOT NULL,
     "isClaimed" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Link_linkIndex_key" ON "Link"("linkIndex");
 
 -- AddForeignKey
 ALTER TABLE "Link" ADD CONSTRAINT "Link_nwcId_fkey" FOREIGN KEY ("nwcId") REFERENCES "NWC"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
