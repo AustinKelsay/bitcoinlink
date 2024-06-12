784f46e065bb5ac678d28ff9b39d7a93fa3dcb76
austinkelsay <austinkelsay@yahoo.com>
2024-06-11 19:52:26 -0500

    Remove old endpoints

--- src/pages/api/nwc/index.js
+++ src/pages/api/nwc/index.js
@@ -9,14 +9,6 @@ export default async function handler(req, res) {
             console.error(error);
             res.status(500).json({ error: 'Internal server error' });
         }
-    } else if (req.method === 'GET') {
-        try {
-            const nwcs = await getAllNwcs();
-            res.status(200).json(nwcs);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
     } else {
         // Handle any other HTTP method
         res.setHeader('Allow', ['GET', 'POST']);
