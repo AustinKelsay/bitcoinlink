098eb6c3cc109711b57e785afee30ee29661b1df
austinkelsay <austinkelsay@yahoo.com>
2024-06-16 12:12:35 -0500

    reuse global prisma client instance in dev and prod

--- src/models/prisma.js
+++ src/models/prisma.js
@@ -1,25 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 
 // Declare a variable to hold our Prisma client instance.
 let prisma;
 
-// Check if the application is running in a production environment.
-if (process.env.NODE_ENV === 'production') {
-    // In production, always create a new instance of PrismaClient.
-    prisma = new PrismaClient();
-} else {
-    // In development or other non-production environments...
-
-    // Check if there is already an instance of PrismaClient attached to the global object.
-    if (!global.prisma) {
-        // If not, create a new instance of PrismaClient...
-        global.prisma = new PrismaClient();
-    }
-    // ...and assign it to the prisma variable. This ensures that in development,
-    // the same instance of PrismaClient is reused across hot reloads and server restarts,
-    // which can help prevent the exhaustion of database connections during development.
-    prisma = global.prisma;
+// Check if there is already an instance of PrismaClient attached to the global object.
+// If not, create a new instance of PrismaClient and attach it to the global object.
+// This ensures that the same instance of PrismaClient is reused across multiple invocations.
+if (!global.prisma) {
+    global.prisma = new PrismaClient();
 }
 
+// Assign the global PrismaClient instance to the prisma variable.
+prisma = global.prisma;
+
 // Export the prisma client instance, making it available for import in other parts of the application.
-export default prisma;
+module.exports = prisma;
