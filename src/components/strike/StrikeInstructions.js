1b222359f19c4f97caff6464abde5e610d5f5481
austinkelsay <austinkelsay@yahoo.com>
2024-09-03 18:37:51 -0500

    fix image

--- src/components/strike/StrikeInstructions.js
+++ src/components/strike/StrikeInstructions.js
@@ -15,7 +15,7 @@ const StrikeInstructions = ({ isVisible, onHide, input, setInput, onSubmit }) =>
         <Dialog header="Strike Instructions" visible={isVisible} onHide={onHide} maximizable>
             <p>Locate your Lightning address (found in the top right corner of the app) and input it into the field below.</p>
             <div className="flex flex-row justify-evenly gap-2">
-                <Image src="/strike0.png" alt="Strike Instructions" width={300} height={650} />
+                <Image src="/strike0.jpeg" alt="Strike Instructions" width={300} height={650} />
                 <div className='flex flex-col gap-2'>
                     <InputText className='h-fit w-full' placeholder="Enter your Lightning Address" value={input} onChange={(e) => setInput(e.target.value)} />
                     <Button severity='success' className='h-fit w-full' label="Submit" onClick={(e) => handleSubmit(e)} />
